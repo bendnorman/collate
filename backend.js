52678dacf44bbecca5cefc802f5ba27f5339032e
bendnorman <bdn29@cornell.edu>
2019-04-11 12:56:03 -0400

    reorg and server creation

---
+++ backend.js
@@ -0,0 +1,56 @@
+const map = L.map('map').setView([40.75580565, -73.95491257451251], 15);
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png', {
+    maxZoom: 18
+}).addTo(map);
+
+// Adding Voyager Labels
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_only_labels/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    zIndex: 10
+}).addTo(map);
+
+var client = new carto.Client({
+    apiKey: '9d8dfbe609f8a21a07aada09014da359eeccefd9',
+    username: 'bendnorman'
+});
+
+const zillowNeighborhoodsDataset = new carto.source.Dataset(`
+    zillowneighborhoods_ny
+  `);
+
+const zillowNeighborhoodsStyle = new carto.style.CartoCSS(`
+    #layer {
+    polygon-fill: #162945;
+      polygon-opacity: 0.5;
+      ::outline {
+        line-width: 1;
+        line-color: #FFFFFF;
+        line-opacity: 0.5;
+      }
+    }
+  `);
+const zillowNeighborhoods = new carto.layer.Layer(zillowNeighborhoodsDataset, zillowNeighborhoodsStyle);
+
+client.addLayers([zillowNeighborhoods]);
+client.getLeafletLayer().addTo(map);
+
+//// Project Profile
+function getProjectData(form) {
+    var address = form.inputbox.value;
+    console.log(address)
+    const Http = new XMLHttpRequest();
+    const url = 'http://localhost:8080/?address=' + address;
+    Http.open("GET", url, true);
+    Http.send();
+
+    Http.onerror = function() {
+        console.log('There was an error!');
+    };
+
+    // Get data back
+    Http.onload = function() {
+        if (Http.readyState == XMLHttpRequest.DONE) {
+            console.log(Http.responseText)
+        }
+    }
+}
---
+++ python_server/server.py
@@ -0,0 +1,43 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer, SimpleHTTPRequestHandler, test
+from urllib.parse import urlparse
+
+from io import BytesIO
+
+
+PORT_NUMBER = 8080
+
+# This class will handles any incoming request from
+# the browser
+
+class CORSHTTPRequestHandler(SimpleHTTPRequestHandler):
+
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        super(CORSHTTPRequestHandler, self).end_headers()
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        
+        address = urlparse(self.path).query.split('=')[1]
+        print(address)
+        
+        response = BytesIO()
+        response.write(b'This is POST request. ')
+        print(response.getvalue())
+        self.wfile.write(response.getvalue())
+    
+    
+try:
+    # Create a web server and define the handler to manage the
+    # incoming request
+    server = HTTPServer(('', PORT_NUMBER), CORSHTTPRequestHandler)
+    print(f'Started httpserver on port {PORT_NUMBER}')
+
+    # Wait forever for incoming htto requests
+    server.serve_forever()
+
+except KeyboardInterrupt:
+    print(f'^C received, shutting down the web server')
+    server.socket.close()
